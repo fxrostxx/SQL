72e145348c17e7591b932369f344daaf4a525a7b
fxrostxx <fxrostxx@FXROSTXX-PC>
2026-02-18 21:47:26 +0300

    SQLQuery5-COUNT_GROUPS_STUDENTS fixed

--- DML/SQLQuery5-COUNT_GROUPS_STUDENTS.sql
+++ DML/SQLQuery5-COUNT_GROUPS_STUDENTS.sql
@@ -2,10 +2,16 @@
 
 USE PV_521_Import;
 
+--SELECT
+--       direction_name           AS N'Направление обучения',
+--       COUNT(DISTINCT group_id) AS N'Количество групп',
+--       COUNT(stud_id)           AS N'Количество студентов'
+--FROM Directions, Groups, Students
+--WHERE [group] = group_id AND direction = direction_id
+--GROUP BY direction_name;
+
 SELECT
-       direction_name  AS N'Направление обучения',
-       COUNT(group_id) AS N'Количество групп',
-       COUNT(stud_id)  AS N'Количество студентов'
-FROM Directions, Groups, Students
-WHERE [group] = group_id AND direction = direction_id
-GROUP BY direction_name;
+       direction_name                   AS N'Направление обучения',
+       (SELECT COUNT(DISTINCT group_id) FROM Groups             WHERE direction = direction_id)                        AS N'Количество групп',
+       (SELECT COUNT(DISTINCT stud_id)  FROM Students, Groups   WHERE [group] = group_id AND direction = direction_id) AS N'Количество студентов'
+FROM Directions;
