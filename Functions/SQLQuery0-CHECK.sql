394dd677d1c74e8f2998f7dabe5146c38a9aabba
fxrostxx <fxrostxx@FXROSTXX-PC>
2026-03-03 19:51:06 +0300

    SQLQuery0-CHECK for PV_319

--- Functions/SQLQuery0-CHECK.sql
+++ Functions/SQLQuery0-CHECK.sql
@@ -4,5 +4,5 @@ USE PV_521_Import;
 SET DATEFIRST 1;
 GO
 
-EXEC SP_SelectScheduleFor N'PV_521';
-PRINT dbo.GetNextStudyDay(N'PV_521');
+EXEC SP_SelectScheduleFor N'PV_319';
+PRINT dbo.GetNextStudyDay(N'PV_319');
