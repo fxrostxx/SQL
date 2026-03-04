5a3955efc6f75fc1fda0d1881e72e711c9ab0dd5
fxrostxx <fxrostxx@FXROSTXX-PC>
2026-03-04 20:30:56 +0300

    GetNextStudyDate DONE

--- Functions/SQLQuery0-CHECK.sql
+++ Functions/SQLQuery0-CHECK.sql
@@ -4,5 +4,8 @@ USE PV_521_Import;
 SET DATEFIRST 1;
 GO
 
-EXEC SP_SelectScheduleFor N'PV_319';
-PRINT dbo.GetNextStudyDay(N'PV_319');
+EXEC SP_SelectScheduleFor N'PV_521';
+--PRINT dbo.GetNextStudyDay(N'PV_521', DEFAULT);
+--PRINT dbo.GetNextStudyDate(N'PV_521', DEFAULT);
+PRINT dbo.GetNextStudyDay(N'PV_521', N'2026-03-06');
+PRINT dbo.GetNextStudyDate(N'SPU_411', N'2026-03-07');
--- Functions/SQLQuery2-GET_NEXT_STUDY_DAY.sql
+++ Functions/SQLQuery2-GET_NEXT_STUDY_DAY.sql
@@ -4,15 +4,15 @@ USE PV_521_Import;
 SET DATEFIRST 1;
 GO
 
-CREATE OR ALTER FUNCTION GetNextStudyDay(@group_name AS NCHAR(10)) RETURNS TINYINT
+CREATE OR ALTER FUNCTION GetNextStudyDay(@group_name AS NCHAR(10), @last_date AS DATE = N'1900-01-01') RETURNS TINYINT
 AS
 BEGIN
-	DECLARE @group_id  AS INT     = (SELECT group_id    FROM Groups   WHERE group_name = @group_name);
-	DECLARE @weekdays  AS TINYINT = (SELECT weekdays    FROM Groups   WHERE group_id = @group_id);
-	DECLARE @last_date AS DATE    = (SELECT MAX([date]) FROM Schedule WHERE [group] = @group_id);
-	DECLARE @last_day  AS TINYINT = DATEPART(WEEKDAY, @last_date);
-	DECLARE @day       AS TINYINT = @last_day + 1;
-	DECLARE @counter   AS TINYINT = 0;
+	DECLARE @group_id AS INT     = (SELECT group_id    FROM Groups   WHERE group_name = @group_name);
+	DECLARE @weekdays AS TINYINT = (SELECT weekdays    FROM Groups   WHERE group_id = @group_id);
+	IF @last_date = CAST(N'1900-01-01' AS DATE) SET @last_date = (SELECT MAX([date]) FROM Schedule WHERE [group] = @group_id);
+	DECLARE @last_day AS TINYINT = DATEPART(WEEKDAY, @last_date);
+	DECLARE @day      AS TINYINT = @last_day + 1;
+	DECLARE @counter  AS TINYINT = 0;
 
 	WHILE @counter < 7
 	BEGIN
@@ -25,13 +25,16 @@ END
 
 GO
 
-CREATE OR ALTER FUNCTION GetNextStudyDate(@group AS INT, @date AS DATE) RETURNS DATE
+CREATE OR ALTER FUNCTION GetNextStudyDate(@group_name AS NCHAR(10), @date AS DATE = N'1900-01-01') RETURNS DATE
 AS
 BEGIN
-	WHILE 1 = 1
-	BEGIN
-		SET @date = DATEADD(DAY, 1, @date);
-		IF DATEPART(WEEKDAY, @date) = dbo.GetNextStudyDay(@group, @date) RETURN @date;
-	END
-	RETURN @date;
+	DECLARE @group_id  AS INT        = (SELECT group_id    FROM Groups   WHERE group_name = @group_name);
+	IF @date = CAST(N'1900-01-01' AS DATE) SET @date = (SELECT MAX([date]) FROM Schedule WHERE [group] = @group_id);
+	DECLARE @day       AS SMALLINT   = DATEPART(WEEKDAY, @date);
+	DECLARE @next_day  AS SMALLINT   = dbo.GetNextStudyDay(@group_name, @date);
+	DECLARE @interval  AS SMALLINT   = @next_day - @day;
+	IF @interval < 0   SET @interval = 7 + @interval;
+	IF @interval = 0   SET @interval = 7;
+	DECLARE @next_date AS DATE       = DATEADD(DAY, @interval, @date);
+	RETURN @next_date;
 END
